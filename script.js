783f09d8667f2b81272df8da22124950a08690c0
Shelke12 <pallavi.21910309@viit.ac.in>
2023-08-16 20:06:55 +0530

    some media queries

---
+++ script.js
@@ -0,0 +1,9 @@
+function getInput() {
+    const el1 = document.getElementsByClassName(bth_calss);
+    const firstInput = el1.value;
+}
+
+function showInput() {
+    getInput();
+    const input1 = getInput().value
+}
