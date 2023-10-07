34a5bbc366bae157c82360c27d7ad7a412e4a53e
Shelke12 <pallavi.21910309@viit.ac.in>
2023-10-07 14:34:18 +0530

    completed

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-var number = "";
-
-input = document.querySelectorAll('button')
-
+let number = "";
+let input = document.querySelectorAll('button')
 input.forEach((item) => {
     item.addEventListener('click', (clickMe) => {
         if (clickMe.target.innerHTML == '=') {
             number = eval(number)
-            document.getElementById("input_feild").value += clickMe.target.innerHTML
+            document.getElementById("input_feild").value = number
+
+        } else if (clickMe.target.innerHTML == 'x') {
+            clickMe.target.innerHTML = '*';
+            number += clickMe.target.innerHTML
+            document.getElementById("input_feild").value = number
+        } else if (clickMe.target.innerHTML == 'RESET') {
+            number = ""
+            document.getElementById("input_feild").value = number
+        } else if (clickMe.target.innerHTML == 'DEL') {
+            number = number.slice(0, -1)
+            document.getElementById("input_feild").value = number;
 
         } else {
-            number = number + clickMe.target.innerHTML
-            document.getElementById("input_feild").value += number
+            number += clickMe.target.innerHTML
+            document.getElementById("input_feild").value = number
         }
     })
 
